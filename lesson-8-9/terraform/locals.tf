01b0f822fa738e24f14954fc0c18da28dbaa2e1a
ZorAnderius <gabriel.zorander@gmail.com>
2025-07-19 09:05:00 +0100

    add: files and folders from previos HW

--- lesson-8-9/terraform/locals.tf
+++ lesson-8-9/terraform/locals.tf
@@ -1,18 +1,18 @@
 locals {
   #S3 bucket and MongoDB
-  bucket_name     = "tf-lesson5-bucket"
+  bucket_name     = "tf-lesson9-bucket"
   table_name      = "tf-locks"
 
   region          = "eu-central-1"
   #VPC
-  vpc_name        = "lesson-5"
+  vpc_name        = "lesson-9"
   vpc_cidr_block  = "10.0.0.0/16"
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   
   #ECR
-  ecr_name        = "lesson-7-chart"
+  ecr_name        = "lesson-9-chart"
 
   #EKS
   cluster_name = "eks-cluster-demo"
